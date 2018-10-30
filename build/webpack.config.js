246c18d3a5276b4507002e7e39a15b54a950641f
magicxin <xin_sui@yahoo.com>
2018-10-30 16:44:11 +0800

    webpack.config

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -15,6 +15,29 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader'
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+        	loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader','css-loader','sass-loader']
+      },
     ]
   },
   resolve: {
